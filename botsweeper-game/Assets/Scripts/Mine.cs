f821e19319623f0e4a5dad7cc489fcf59160eba0
Antton Hytönen <anttonhytonen@kamk.fi>
2017-03-29 10:36:21 +0300

    Initial commit

---
+++ botsweeper-game/Assets/Scripts/Mine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mine : MonoBehaviour
+{
+    public Color NeutralColor;
+    public Color GoodColor;
+    public Color BadColor;
+
+    public Image CurrentColorImage;
+    public Image NewColorImage;
+
+    private Animator mAnimator;
+
+    public void SwitchColorStart(bool good)
+    {
+        NewColorImage.color = good ? GoodColor : BadColor;
+        mAnimator.Play("Change");
+    }
+
+    public void SwitchColorEnd()
+    {
+        CurrentColorImage.color = NewColorImage.color;
+    }
+
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+}
---
+++ botsweeper-game/Assets/Scripts/Minefield.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minefield : MonoBehaviour
+{
+    public GameObject MinePrefab;
+
+    private RectTransform mTransform;
+
+    public void GenerateField(int width, int height)
+    {
+        foreach(Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+
+        for(int y = 0; y < height; y++)
+            for(int x = 0; x < width; x++)
+            {
+
+            }
+    }
+
+    void Start()
+    {
+        mTransform = GetComponent<RectTransform>();
+    }
+}

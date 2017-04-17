27505bf4a8a39364532b410f6b89c08f6e326d12
Antton Hytönen <hytonen-a@hotmail.com>
2017-04-17 17:10:53 +0300

    Game server things

---
+++ botsweeper-game/Assets/Scripts/Protocol.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace Protocol
+{
+	public enum BaseMessageType
+	{
+		ListGamesRequest = 1,
+		ListGamesResponse,
+
+		CreateGameRequest,
+		CreateGameResponse,
+
+		RemoveGameRequest,
+		RemoveGameResponse,
+
+		EndpointMessage,
+		EndpointMessageError,
+	}
+
+	public enum MessageType
+	{
+		JoinGameRequest = 1,
+		JoinGameResponse,
+
+		LeaveGameRequest,
+		LeaveGameResponse,
+
+		VoteAvailable,
+		VoteResult,
+		Vote,
+
+		MineSweepResult,
+	}
+
+	public class Message
+	{
+		public string from_id { get; set; }
+		public string to_id { get; set; }
+		public Dictionary<object, object> message { get; set; }
+	}
+
+	public class BaseMessage
+	{
+		public BaseMessage()
+		{
+			type = BaseMessageType.EndpointMessage;
+		}
+
+		public BaseMessageType type { get; set; }
+
+		public string name { get; set; }
+		public Message message { get; set; }
+	}
+}
---
+++ botsweeper-game/Assets/Scripts/TCPServer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.Text;
+
+public class TCPServer : MonoBehaviour
+{
+	public string Address;
+	public int Port;
+
+	private TcpClient mClient;
+
+	IEnumerator CreateGame()
+	{
+		var msg = new Protocol.BaseMessage ();
+		msg.type = Protocol.BaseMessageType.CreateGameRequest;
+		msg.name = "My game";
+		var data = JsonConvert.SerializeObject (msg);
+		mClient.Client.Send (Encoding.Unicode.GetBytes(data));
+		// TODO
+		return null;
+	}
+
+	public void Connect()
+	{
+		mClient = new TcpClient ();
+		mClient.Connect (Address, Port);
+		StartCoroutine (CreateGame ());
+	}
+}
